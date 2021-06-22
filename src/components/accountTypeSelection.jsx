c3f1bdbf403a23d98ce0e730540d67e8d6d2889a
Amaaz <amaaz@pop-os.localdomain>
2021-06-22 10:07:58 +0500

    landing page styling added

--- src/components/accountTypeSelection.jsx
+++ src/components/accountTypeSelection.jsx
@@ -3,18 +3,24 @@ import { Button } from "react-bootstrap";
 import { Row, Container } from "react-bootstrap";
 
 import { useHistory } from "react-router";
+import userIcon from './../images/userIcon.png'
+import restaurantIcon from './../images/restaurantsIcon.png'
 
 function AccountTypeSelection(props) {
   const history = useHistory();
 
   return (
     <React.Fragment>
-      <Container>
+      <Container style={{display:"flex", width:"100vw", height:"100vh", flexDirection:"column", justifyContent:"center", alignItems:"center"} }>
         <Row>
-          <Button onClick={() => history.push("/userProfile")}>User</Button>
+          <Button style={{width:"400px", height:"200px", marginBottom:"50px"}} onClick={() => history.push("/userProfile")}>
+            <img style={{width:'150px', height:'fit-content'}} src={userIcon} alt=""></img>
+            User
+            </Button>
         </Row>
         <Row>
-          <Button onClick={() => history.push("restaurantProfile")}>
+          <Button style={{width:"400px", height:"200px", marginBottom:"50px"}} onClick={() => history.push("restaurantProfile")}>
+            <img style={{width:'150px', height:'fit-content'}} src={restaurantIcon} alt=""></img>
             Restaurant
           </Button>
         </Row>
